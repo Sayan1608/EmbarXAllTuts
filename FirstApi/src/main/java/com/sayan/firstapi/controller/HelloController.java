76ae908c8a667d0a0f668f8a07540012b827eeec
sayan <subhosen16@gmail.com>
2025-12-26 17:41:46 +0530

    Created Post Mapping in HelloController

--- FirstApi/src/main/java/com/sayan/firstapi/controller/HelloController.java
+++ FirstApi/src/main/java/com/sayan/firstapi/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.sayan.firstapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping(path = "/hello")
     public String sayHello(){
         return "Hello World!";
+    }
+
+    @PostMapping(path = "/hello")
+    public String getPost(@RequestBody String name){
+        return "Hello "+name + "!";
     }
 }
