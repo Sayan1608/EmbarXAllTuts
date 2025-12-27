ec8705cc553ace96cf018b16b5ef0ae8985be5af
sayan <subhosen16@gmail.com>
2025-12-27 19:48:00 +0530

    Added and Implemented Category Service

--- sb-ecommerce/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecommerce/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,27 +1,31 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    private List<Category> categoryList = new ArrayList<>();
+    private final CategoryServiceImpl categoryService;
+
+    public CategoryController(CategoryServiceImpl categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping(path = "/api/public/categories")
     public List<Category> getAllCategories(){
-        return categoryList;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping(path = "/api/public/categories")
     public String createNewCategory(@RequestBody Category category){
-        categoryList.add(category);
+       categoryService.createNewCategory(category);
         return "Category Added Successfully!";
     }
 }
