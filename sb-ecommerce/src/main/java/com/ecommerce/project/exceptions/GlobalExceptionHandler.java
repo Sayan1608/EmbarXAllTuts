4c27c0e13ce8a4052ac1acaeed7ac9fa713d4a58
sayan <subhosen16@gmail.com>
2026-01-07 21:28:14 +0530

    Added Custom Exception Handling to provide user friendly validation messages

---
+++ sb-ecommerce/src/main/java/com/ecommerce/project/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ecommerce.project.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String,String> response = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(err->{
+          String field = ((FieldError)err).getField();
+          String message = err.getDefaultMessage();
+          response.put(field,message);
+        });
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
